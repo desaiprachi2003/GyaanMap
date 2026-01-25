f14926511a9bcc56110420a4f049c8f4d938b9da
desaiprachi2003 <desaiprachi296@gmail.com>
2026-01-25 10:07:08 +0530

    updated backend

--- Backend/backend_roadmap/main.py
+++
@@ -1,8 +0,0 @@
-# backend/main.py
-from fastapi import FastAPI
-
-app = FastAPI(title="GyaanMap Roadmap API")
-
-@app.get("/")
-async def root():
-    return {"message": "GyaanMap Roadmap API is running"}
